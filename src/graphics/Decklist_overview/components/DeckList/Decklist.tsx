afe75c2309f4c7310f122ce94936bfcc70793785
Hectorban <hectorban1234@gmail.com>
2021-06-26 01:47:15 -0500

    decklist labels

--- src/graphics/Decklist_overview/components/DeckList/Decklist.tsx
+++ src/graphics/Decklist_overview/components/DeckList/Decklist.tsx
@@ -8,6 +8,15 @@ interface Props {
 
 const Decklist:FC<Props> = ({deck}:Props) => (
     <div id='decklist-container'>
+        <div className='decklist -firstcolumn'>
+            <div className='decklist-column-text'>Unidades</div>
+        </div>
+        <div className='decklist -secondcolumn'>
+            <div className='decklist-column-text'>Hechizos</div>
+        </div>
+        <div className='decklist -thirdcolumn'>
+            <div className='decklist-column-text'>Hitos</div>
+        </div>
         {deck.map((card, i) => {
             const {code, count, faction} = card
              return (
