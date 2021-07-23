f41b6003de4a598e8aa8cd3999875f6041fd9a1f
Hectorban <hectorban1234@gmail.com>
2021-07-22 20:33:13 -0500

    updated some of the backend code with cleaner solutions

--- src/extension/ddragonfetch.ts
+++ src/extension/ddragonfetch.ts
@@ -5,26 +5,26 @@ import { DDcardTypes } from '~types/ddragon_cardTypes'
 
 const nodecg = nodecgApiContext.get()
 const cardDataRep = nodecg.Replicant('ddCardData')
-// This algo is a tower of doom, i hate it, but it's the best way i found to consume and parse the colossal ammount of cards that LoR provides
-fetch("https://dd.b.pvp.net/latest/core/es_mx/data/globals-es_mx.json")
-.then(res => res.json())
-.then(async (response: GlobalDatatypes) => {
+
+export default async function ddragonFetch():Promise<void> {
+	const globalsFetch = await fetch("https://dd.b.pvp.net/latest/core/es_mx/data/globals-es_mx.json")
+	const globalsData:GlobalDatatypes = await globalsFetch.json()
 	const cardData = {}
 	await Promise.all(
-		response.sets.map(async (set, i) => {
-			nodecg.log.info(`Fetching ${set.name} data...`)
-			await fetch(`http://dd.b.pvp.net/latest/set${i+1}/es_mx/data/set${i+1}-es_mx.json`)
-			.then(res => res.json())
-			.then((response: [DDcardTypes]) => {
-				response.map((card) => {
+		globalsData.sets.map(async (set, i) => {
+			try {
+				nodecg.log.info(`Fetching ${set.name} data`)
+				const setFetch = await fetch(`http://dd.b.pvp.net/latest/set${i+1}/es_mx/data/set${i+1}-es_mx.json`)
+				const setData:[DDcardTypes] = await setFetch.json()
+				setData.forEach((card) => {
 					cardData[card.cardCode] = {
 						"name": card.name,
 						"cost": card.cost,
 						"type": card.type,
 						"supertype": card.supertype
-					}	
-				})
-			})
+					}
+				}) 
+			} catch(error){nodecg.log.error(`Set ${set.name} doesn't exists`)}
 		})
 	)
 	cardDataRep.value = cardData
@@ -33,4 +33,5 @@ fetch("https://dd.b.pvp.net/latest/core/es_mx/data/globals-es_mx.json")
             ack(null, cardData[value])
         }
     })
-})
+}
+ddragonFetch()
