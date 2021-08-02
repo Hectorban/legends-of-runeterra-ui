08f21978a40734a0966679accd23d374f9cb719a
Hectorban <31503660+Hectorban@users.noreply.github.com>
2021-08-02 14:40:10 -0500

    Changed the fetching algorithm to async

--- src/extension/ddragonfetch.ts
+++ src/extension/ddragonfetch.ts
@@ -1,36 +1,41 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch"
 import * as nodecgApiContext from './utils/nodecg-api-context'
-import { GlobalDatatypes } from '~types/ddragon_globalTypes'
+import { GlobalDatatypes } from "~types/ddragon_globalTypes"
 import { DDcardTypes } from '~types/ddragon_cardTypes'
 
 const nodecg = nodecgApiContext.get()
 const cardDataRep = nodecg.Replicant('ddCardData')
-// This algo is a tower of doom, i hate it, but it's the best way i found to consume and parse the colossal ammount of cards that LoR provides
-fetch("https://dd.b.pvp.net/latest/core/es_mx/data/globals-es_mx.json")
-.then(res => res.json())
-.then(async (response: GlobalDatatypes) => {
+
+const main = async () => {
+	const globalsReq = await fetch('https://dd.b.pvp.net/latest/core/es_mx/data/globals-es_mx.json')
+	const globalsData:GlobalDatatypes = await globalsReq.json()
 	const cardData = {}
 	await Promise.all(
-		response.sets.map(async (set, i) => {
+		globalsData.sets.map(async (set, i) => {
 			nodecg.log.info(`Fetching ${set.name} data...`)
-			await fetch(`http://dd.b.pvp.net/latest/set${i+1}/es_mx/data/set${i+1}-es_mx.json`)
-			.then(res => res.json())
-			.then((response: [DDcardTypes]) => {
-				response.map((card) => {
-					cardData[card.cardCode] = {
-						"name": card.name,
-						"cost": card.cost,
-						"type": card.type,
-						"supertype": card.supertype
-					}	
-				})
+			try {
+			const setRequest = await fetch(`http://dd.b.pvp.net/latest/set${i+1}/es_mx/data/set${i+1}-es_mx.json`)
+			const setData:[DDcardTypes] = await setRequest.json()
+			setData.map((card) => {
+				cardData[card.cardCode] = {
+					"name": card.name,
+					"cost": card.cost,
+					"type": card.type,
+					"supertype": card.supertype
+				}
 			})
+			}
+			catch {
+				nodecg.log.error(`${set.name} doesn't exist in the data base`)
+			}
 		})
 	)
 	cardDataRep.value = cardData
-    nodecg.listenFor("ddcardData", (value, ack) => {
-        if(ack && !ack.handled) {
-            ack(null, cardData[value])
-        }
-    })
-})
+	nodecg.listenFor("ddcardData", (value, ack) => {
+		if(ack && !ack.handled) {
+			ack(null, cardData[value])
+		}
+	})
+}
+
+main()
